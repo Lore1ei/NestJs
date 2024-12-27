0066247ce4b6d92450c0fd74056147efe21206a9
Lore1ei <harubator@gmail.com>
2024-12-27 11:10:37 +0700

    page 33

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerModule } from './common/middleware/logger/logger.module';
 
 @Module({
-  imports: [SongsModule],
+  imports: [SongsModule, LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
