9d024fff449c32538ff4e0213b35deaab64cb427
Lore1ei <harubator@gmail.com>
2024-12-28 12:06:42 +0700

    page 46

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { SongsController } from './songs.controller';
-import { SongsService } from './songs.service';
+import { SongsController } from "./songs.controller";
+import { Module } from "@nestjs/common";
+import { SongsService } from "./songs.service";
 
+const mockSongsService = {
+  findAll() {
+    return [{
+      id: 1,
+      title: "Lasting lover"
+    }];
+  },
+};
 @Module({
   controllers: [SongsController],
-  providers: [SongsService]
+  providers: [
+    SongsService,
+    {
+      provide: SongsService,
+      useValue: mockSongsService,
+    },
+  ],
 })
 export class SongsModule {}
