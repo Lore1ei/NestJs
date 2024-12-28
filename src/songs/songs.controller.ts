c5851ad952538b650973765975833d8bc9d2dd77
Lore1ei <harubator@gmail.com>
2024-12-28 10:33:15 +0700

    page 43

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Delete, Get, HttpException, HttpStatus, Post, Put } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpException,
+    HttpStatus,
+    Param,
+    ParseIntPipe,
+    Post,
+    Put
+} from "@nestjs/common";
 import { SongsService } from "./songs.service";
+import { CreateSongDto } from "./dto/create-song.dto";
 
 @Controller('songs')
 export class SongsController {
     constructor(private readonly songsService: SongsService) {}
 
     @Post()
-    create(){
-        return this.songsService.create('Animals by Martin');
+    create(@Body() createSongDto: CreateSongDto) {
+        const result = this.songsService.create(createSongDto)
+        return result
     }
 
     @Get()
@@ -21,8 +34,13 @@ export class SongsController {
             );
         }
     }
+
     @Get(":id")
-    findOne(){
+    findOne(
+      @Param('id', new ParseIntPipe({
+          errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+      })) id: number,
+    ){
         return "fetch song on the based on id";
     }
     @Put(":id")
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreateSongDto } from "./dto/create-song.dto";
 
 @Injectable()
 export class SongsService {
   private readonly songs = [];
 
-  create(song: string){
+  create(song: CreateSongDto) {
     this.songs.push(song);
     return this.songs;
   }
-  findAll(){
+  findAll() {
     return this.songs;
   }
 }
