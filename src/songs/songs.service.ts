90345a4946f878c9a3473578c60b1f68f63a7ce9
Lore1ei <harubator@gmail.com>
2024-12-28 20:55:08 +0700

    page 50

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from "@nestjs/common";
 import { CreateSongDto } from "./dto/create-song.dto";
+import { Connection } from "../constants/connection";
 
 @Injectable()
 export class SongsService {
+  constructor(
+    @Inject("CONNECTION")
+    connection: Connection
+  ) {
+    console.log(connection.CONNECTION_STRING);
+  }
+
   private readonly songs = [];
 
   create(song: CreateSongDto) {
